4ad737d635d467b8c663c850de05fa88124584bf
jay15 <148239272+jay15git@users.noreply.github.com>
2025-08-14 23:23:41 +0530

    resp_to_ping

--- app/main.go
+++ app/main.go
@@ -21,7 +21,7 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
@@ -33,12 +33,6 @@ func main() {
 			fmt.Println("Error:", err)
 			continue
 		}
-
-		// Handle client connection
-		handleClient(conn)
+		conn.Write([]byte("PONG\r\n"))
 	}
-}
-
-func handleClient(conn net.Conn) {
-	conn.Write([]byte("+PONG\r\n"))
 }
