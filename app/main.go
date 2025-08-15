84d968457c9851b9bf159985a24e44a7cc8b595f
jay15 <148239272+jay15git@users.noreply.github.com>
2025-08-15 10:53:34 +0530

    resp_multiple_pings

--- app/main.go
+++ app/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
-
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -21,15 +18,35 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	fmt.Println("Redis server listening on port 6379")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
+		handleConnection(conn)
 	}
-	conn.Write([]byte("+PONG\r\n"))
-	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	for {
+		buff := make([]byte, 1024)
+
+		_, err := conn.Read(buff)
+
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("client disconnected")
+			} else {
+				fmt.Println("error reading from client: ", err.Error())
+			}
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
